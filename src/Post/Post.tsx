72edf8ee1b3bdc865c5639ca51bfee4469532c43
Timofeev Yury <yurik-007@mail.ru>
2023-12-30 01:28:30 +0300

    commit-3

--- src/Post/Post.tsx
+++ src/Post/Post.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Post = () => {
     return (
         <div>
-            <h1>Post1</h1>
+            <h1>Post333</h1>
         </div>
     );
 };
--- src/User/User.tsx
+++ src/User/User.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const User = () => {
     return (
         <div>
-            <h1>User3</h1>
+            <h1>User444</h1>
         </div>
     );
 };
