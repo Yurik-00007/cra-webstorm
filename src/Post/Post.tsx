4bc4d5cca9133c36b2881410f65eb34c3ab6a090
Timofeev Yury <yurik-007@mail.ru>
2024-01-01 22:19:09 +0300

    add function of sum

--- src/Post/Post.tsx
+++ src/Post/Post.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const Post = () => {
+    const sum=(a:number,b:number)=>{
+        return a+b
+    }
     return (
         <div>
             <h1>Post3333</h1>
+            <button onClick={()=>sum(2,9)}></button>
         </div>
     );
 };
